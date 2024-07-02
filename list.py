8e22e78a3bd10a429411ad23072bfb7162e37d06
dltjdwnscjswo <seongjun9537@naver.com>
2024-07-02 16:16:00 +0900

    20240702.Python
    list.up

---
+++ list.py
@@ -0,0 +1,27 @@
+leeseongjun=["이성준","24","010-6612-****"]
+name=leeseongjun[0]
+age=leeseongjun[1]
+phone=leeseongjun[2]
+
+names=[['강수경','강혜나','김민석'],['20','21','22'],['010-1111-2222','010-2222-3333','010-3333-4444']]
+print(names[0][1])
+
+print(name,age,phone)
+print(type(leeseongjun))
+
+print(names[0][0:2])
+
+nubers=[1,2,3,4,5]
+
+result = nubers[2] + nubers [-1]
+print(result)
+
+print(len(names[0]))
+
+# 리스트에 요소 추가하기
+last=[1,2,3]
+last.append(30)
+print(last)
+last.remove(3)
+print(last)
+print(type(last))
---
+++ number.py
@@ -0,0 +1,12 @@
+string="1"
+int=1
+
+print(string)
+print(int)
+# 해당 문자 인지 숫자인지 확인해주는 함수.
+print(type(int))
+
+number_1=2
+number_2=5
+
+print(number_1+number_2)
